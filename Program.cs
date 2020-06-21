faffd936291d8fcb188dcd1adb08bd7da362098f
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-06-20 21:41:24 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Senha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Olá, usuário. Por favor, digite sua senha: ");
+            string senha=Console.ReadLine();
+
+            Console.Clear();
+
+            if(senha=="1234abcd")
+            Console.WriteLine("Acesso permitido");
+
+            else
+            Console.WriteLine("Acesso negado");
+        }
+    }
+}
